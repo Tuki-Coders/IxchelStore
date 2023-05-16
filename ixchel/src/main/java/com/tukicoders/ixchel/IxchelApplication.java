be47bdf2fb45a2205cff6d06a6e1984ecc00c186
SanchezrEdwin01 <sanchez.r.edwin09@gmail.com>
2023-05-16 01:19:19 -0600

    Initial structure

---
+++ ixchel/src/main/java/com/tukicoders/ixchel/IxchelApplication.java
@@ -0,0 +1,13 @@
+package com.tukicoders.ixchel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IxchelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IxchelApplication.class, args);
+	}
+
+}
